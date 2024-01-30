09363d3b02811e5e11689773b70f00c661166851
Heric Pan <30740714+HericPan@users.noreply.github.com>
2024-01-30 21:29:16 +0000

    finished task.c

--- tasks/task.c
+++ tasks/task.c
@@ -1,14 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
+int main(int argc, char *argv[]) {
     int num1, num2;
+    
+    // Check the number of arguments is 2, otherwise exit program
+    if (argc != 3) {
+        printf("Invalid inputs: the number of arguments should be 2. \n");
+        return -1;
+    }
 
-    // Prompt the user for input
-    printf("Enter the first number: ");
-    scanf("%d", &num1);
+    // Check the arguments can be translated into integers
+    num1 = atoi(argv[1]);
+    num2 = atoi(argv[2]);
+    if (num1 < 1 || num2 < 1) {
+        printf("Invalid inputs: the input numbers should be integer. \n");
+        return -1;
+    }
 
-    printf("Enter the second number: ");
-    scanf("%d", &num2);
+    // // Prompt the user for input
+    // printf("Enter the first number: ");
+    // scanf("%d", &num1);
+
+    // printf("Enter the second number: ");
+    // scanf("%d", &num2);
 
     // Calculate and print the sum
     int sum = num1 + num2;
